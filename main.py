f169eac0fbc5769503e33768b26ddc5f59cdd902
Kamil Polec <kamilpolec14@gmail.com>
2024-03-04 13:11:52 +0000

    improved the method of image composition

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.filedialog import askopenfilename, askopenfilenames
-from PIL import Image, ImageEnhance, ImageTk
-from random import randint
+from tkinter.filedialog import askopenfilenames
+from PIL import Image, ImageTk
 
 
 class App(tk.Tk):
@@ -14,6 +13,7 @@ class App(tk.Tk):
 class Watermark(ttk.Frame):
     def __init__(self, container):
         super().__init__()
+        self.container = container
         self.grid(padx=12, pady=12, column=0, row=0, sticky="N, W, E, S")
 
         self.photo_to_watermark = ttk.Label(self)
@@ -33,42 +33,46 @@ class Watermark(ttk.Frame):
         f_path = askopenfilenames(initialdir="/",
                                   title="Choose Images",
                                   filetypes=(
-                                      ("JPG files", "*.jpg*"), ("JPEG files", "*.jpeg*"), ("PNG files", "*.png*"),
-                                      ("All Files", "*.*"),))
+                                      ("All Files", "*.*"), ("JPG files", "*.jpg*"), ("JPEG files", "*.jpeg*"),
+                                      ("PNG files", "*.png*"),
+                                  ))
 
         for img in f_path:
             self.img_to_watermark = Image.open(img)
             self.img_list.append(self.img_to_watermark)
 
         self.img = Image.open(f_path[0])
-        if self.img.width > 1000 or self.img.height > 1000:
+        print(self.img.width)
+
+        if self.img.width > 2000 or self.img.height > 1000:
+
             width, height = self.img.width / 4, self.img.height / 4
             self.img = self.img.resize((int(width), int(height)))
         else:
             width, height = self.img.width / 4, self.img.height / 4
             self.img = self.img.resize((int(width), int(height)))
+
         self.image = ImageTk.PhotoImage(self.img)
         self.photo_to_watermark['image'] = self.image
 
     def watermark_img(self):
+
         watermark = Image.open("watermark-example.png")
+        watermark_opacity = watermark.copy()
+        watermark_opacity.putalpha(90)
+        watermark.paste(watermark_opacity, watermark)
+
         for img_to_watermark in self.img_list:
-            watermark = watermark.resize(img_to_watermark.size)
-            img_to_watermark.putalpha(256)
-            watermark.putalpha(48)
+            img_to_watermark.putalpha(255)
+            # watermark = watermark.resize((550, 550))
+            img_to_watermark.alpha_composite(im=watermark, dest=(100, 100), source=(0, 0))
+            self.watermarked_imgs.append(img_to_watermark)
 
-            new = Image.alpha_composite(im1=img_to_watermark, im2=watermark)
-
-            brightness_factor = 1.2  # Increase brightness by 50%
-            enhancer = ImageEnhance.Brightness(new)
-            im_watermarked = enhancer.enhance(brightness_factor)
-            self.watermarked_imgs.append(im_watermarked)
-            # im_watermarked.show()
         self.img_to_watermark = self.watermarked_imgs[0]
 
     def save_img(self):
         n = 1
-        for img in self.watermarked_imgs:
+        for index, img in enumerate(self.watermarked_imgs):
             img.save(fp=f"Watermarked_images/watermarked_image{n}.png")
             n += 1
 
