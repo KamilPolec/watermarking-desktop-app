e1ed327b53b6d8823b61deb411b5935eabcda46f
Kamil Polec <kamilpolec14@gmail.com>
2024-03-07 12:59:00 +0000

    User can now see images in a 3x3 box, including watermarked versions

--- main.py
+++ main.py
@@ -18,24 +18,33 @@ class Watermark(ttk.Frame):
         self.container = container
         self.grid(padx=12, pady=12, column=0, row=0, sticky="N, W, E, S")
         # thumbnail image
+        self.thumbnail_img_list = []
         self.thumbnail_img = Image.open("placeholder.png")
         self.thumbnail_img.thumbnail(THUMBNAIL_SIZE)
         self.thumbnail_img = ImageTk.PhotoImage(self.thumbnail_img)
-        for n in range(0, 3):
+        col = 0
+        row = -1
+        for n in range(0, 9):
+            if n % 3 == 0:
+                row += 1
+                col = 0
             self.thumbnail_label = ttk.Label(self, name=f"thumbnail{n + 1}")
-            self.thumbnail_label.grid(column=n, row=0)
+            self.thumbnail_label.grid(column=col, row=row)
             self.thumbnail_label['image'] = self.thumbnail_img
+            self.thumbnail_img_list.append(self.thumbnail_img)
+            col += 1
+
         # Image being manipulated by Pillow
         self.img_to_watermark = Image.open("placeholder.png")
         self.img_list = []
         self.watermarked_imgs = []
-        self.thumbnail_img_list = []
         # Buttons
-        ttk.Button(self, text="Upload Image", command=self.browse).grid(column=0, row=1, sticky="S")
-        ttk.Button(self, text="Watermark", command=self.watermark_img).grid(column=1, row=1, sticky="S")
-        ttk.Button(self, text="Save Image", command=self.save_img).grid(column=2, row=1, sticky="S")
+        ttk.Button(self, text="Upload Image", command=self.browse).grid(column=0, row=3, sticky="S")
+        ttk.Button(self, text="Watermark", command=self.watermark_img).grid(column=1, row=3, sticky="S")
+        ttk.Button(self, text="Save Image", command=self.save_img).grid(column=2, row=3, sticky="S")
 
     def browse(self):
+        new_images = []
         f_path = askopenfilenames(initialdir="/",
                                   title="Choose Images",
                                   filetypes=(
@@ -46,25 +55,12 @@ class Watermark(ttk.Frame):
         for index, img_path in enumerate(f_path):
             self.img_to_watermark = Image.open(img_path)
             self.img_list.append(self.img_to_watermark)
+            new_images.append(Image.open(img_path))
 
-            self.thumbnail_img = Image.open(img_path)
-            self.thumbnail_img.thumbnail(THUMBNAIL_SIZE)
-            bg_img = Image.open("placeholder.png").copy()
-
-            overlay_width, overlay_height = self.thumbnail_img.size
-            bg_width, bg_height = bg_img.size
-            offset_x = (bg_width - overlay_width) // 2
-            offset_y = (bg_height - overlay_height) // 2
-
-            bg_img.paste(self.thumbnail_img, (offset_x, offset_y))
-            self.thumbnail_img = bg_img
-            self.thumbnail_img = ImageTk.PhotoImage(self.thumbnail_img)
-            self.thumbnail_img_list.append(self.thumbnail_img)
-            if index + 1 < 4:
-                self.nametowidget(f"thumbnail{index + 1}")['image'] = self.thumbnail_img
+        self.change_thumbnails(new_images)
 
     def watermark_img(self):
-
+        self.watermarked_imgs.clear()
         watermark = Image.open("watermark-example.png")
         watermark_opacity = watermark.copy()
         watermark_opacity.putalpha(90)
@@ -76,13 +72,35 @@ class Watermark(ttk.Frame):
             img_to_watermark.alpha_composite(im=watermark, dest=(100, 100), source=(0, 0))
             self.watermarked_imgs.append(img_to_watermark)
 
-        self.img_to_watermark = self.watermarked_imgs[0]
+        self.change_thumbnails(self.watermarked_imgs)
+        self.img_list.clear()
 
     def save_img(self):
         n = 1
         for index, img in enumerate(self.watermarked_imgs):
             img.save(fp=f"Watermarked_images/watermarked_image{n}.png")
             n += 1
+
+    def change_thumbnails(self, img_list):
+        self.thumbnail_img_list = self.thumbnail_img_list[:9]
+        for n in range(1, 10):
+            self.nametowidget(f"thumbnail{n}")['image'] = self.thumbnail_img_list[0]
+        for index, img in enumerate(img_list):
+            self.thumbnail_img = img
+            self.thumbnail_img.thumbnail(THUMBNAIL_SIZE)
+            bg_img = Image.new("RGB", (250, 250), (221, 221, 221))
+
+            overlay_width, overlay_height = self.thumbnail_img.size
+            bg_width, bg_height = bg_img.size
+            offset_x = (bg_width - overlay_width) // 2
+            offset_y = (bg_height - overlay_height) // 2
+
+            bg_img.paste(self.thumbnail_img, (offset_x, offset_y))
+            self.thumbnail_img = bg_img
+            self.thumbnail_img = ImageTk.PhotoImage(self.thumbnail_img)
+            self.thumbnail_img_list.append(self.thumbnail_img)
+            if index < 9:
+                self.nametowidget(f"thumbnail{index + 1}")['image'] = self.thumbnail_img
 
 
 if __name__ == "__main__":
